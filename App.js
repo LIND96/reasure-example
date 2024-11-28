5bca190e7f45af7c1dc067c30fadaeb8823b1f2b
LIND <malintanhernawanputra.net@gmail.com>
2024-11-28 11:51:51 +0700

    baseline perf

---
+++ App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+
+const App = () => {
+  const [count, setCount] = useState(0)
+  const pressCount = () => {
+    setTimeout(() => {
+      setCount(count + 1)
+    }, 700)
+  }
+  return (
+    <SafeAreaView>
+      <View>
+        <View style={{ alignSelf: 'center' }}>
+          <Text>Home Component</Text>
+        </View>
+
+        <Text>{`Count: ${count}`}</Text>
+        <TouchableOpacity testID='btn1' style={{ width: 100, height: 100, backgroundColor: 'red' }} onPress={pressCount}>
+
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App
---
+++ perfTest/App.perf-test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { jest, test } from '@jest/globals';
+import { fireEvent, screen } from '@testing-library/react-native';
+import { measureRenders } from 'reassure';
+import App from '../App';
+
+jest.setTimeout(600_000);
+
+
+test('App Page test onPress', async () => {
+    const scenario = async () => {
+        const button = screen.getByTestId('btn1');
+        fireEvent.press(button);
+        await screen.findByText('Count: 1', {}, { timeout: 1000 });
+    }
+    await measureRenders(<App />, { scenario, runs: 10 });
+})
