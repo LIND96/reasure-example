0238935f886c70568d1fc340cb2c3e3abb696659
LIND <malintanhernawanputra.net@gmail.com>
2024-11-28 14:20:34 +0700

    Improve Performance

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { useState } from 'react';
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity, FlatList } from 'react-native';
 
 const App = () => {
   const [count, setCount] = useState(0)
   const pressCount = () => {
-    setTimeout(() => {
-      setCount(count + 1)
-    }, 100)
+    setCount(count + 1)
   }
   return (
     <SafeAreaView>
@@ -14,12 +12,17 @@ const App = () => {
         <View style={{ alignSelf: 'center' }}>
           <Text>Home Component</Text>
         </View>
-
         <Text>{`Count: ${count}`}</Text>
-        <TouchableOpacity testID='btn1' style={{ width: 100, height: 100, backgroundColor: 'red' }} onPress={pressCount}>
-
+        <TouchableOpacity testID='btn1' style={{}} onPress={pressCount}>
+          <Text style={{ fontWeight: 'bold' }}>Add Count</Text>
         </TouchableOpacity>
       </View>
+      <FlatList
+        data={Array.from({ length: 200 }, (_, index) => index)}
+        renderItem={({item,index})=>{
+          return <Text>List: {item}</Text>
+        }}
+      />
     </SafeAreaView>
   )
 }
